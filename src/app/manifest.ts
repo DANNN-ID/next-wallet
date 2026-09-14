4c5596df88063f930b2491de4d30b0fe86e29eae
DANNN-ID <ahmadwildan.smakensa@gmail.com>
2026-09-14 18:24:46 +0700

    feat: add web app manifest configuration

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -11,9 +11,14 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: '#ffffff',
     icons: [
       {
-        src: '/icon-512x512.svg',
-        sizes: 'any',
-        type: 'image/svg+xml',
+        src: '/favicon.png',
+        sizes: '512x512',
+        type: 'image/png',
+      },
+      {
+        src: '/favicon.png',
+        sizes: '192x192',
+        type: 'image/png',
       },
     ],
   }
